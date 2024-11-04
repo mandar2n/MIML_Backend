625b684d5b79929922fc125cf931d9ef6c4b44b8
ASH <gitae9999@naver.com>
2024-11-04 19:53:13 +0900

    데이터베이스 접근 오류 수정

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config.settings import DATABASE_URL
+from src.config.settings import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
